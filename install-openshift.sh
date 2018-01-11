15d072d1f005c73df1c2fcf669045b6e1cb987af
Marek Jelen <marek@jelen.biz>
2018-01-11 20:01:31 +0100

    :Fixes for 3.7

--- install-openshift.sh
+++ install-openshift.sh
@@ -2,67 +2,79 @@
 
 ## see: https://www.youtube.com/watch?v=-OOnGK-XeVY
 
-DOMAIN=${DOMAIN:=techdope.io}
-USERNAME=${USERNAME:=gshipley}
+DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
+USERNAME=${USERNAME:="$(whoami)"}
 PASSWORD=${PASSWORD:=password}
 
+SCRIPT_REPO=${SCRIPT_REPO:="http://github.com/gshipley/installcentos"}
+
+echo "******"
+echo "* Your domain is $DOMAIN "
+echo "* Your username is $USERNAME "
+echo "* Your password is $PASSWORD "
+echo "******"
 
 yum install -y epel-release
 
-yum install -y git wget zile nano net-tools
-yum install -y docker
-yum install -y python-cryptography pyOpenSSL.x86_64
+yum install -y git wget zile nano net-tools docker \
+python-cryptography pyOpenSSL.x86_64 python2-pip \
+openssl-devel python-devel httpd-tools NetworkManager python-passlib \
+java-1.8.0-openjdk-headless "@Development Tools"
 
-## WORKAROUND as per https://github.com/openshift/openshift-ansible/issues/3111
-## TODO: yum install ansible
-yum install -y "@Development Tools" python2-pip openssl-devel python-devel
-pip install -Iv ansible==2.3.0.0
+systemctl start NetworkManager
+systemctl enable NetworkManager
 
-mkdir -p ~/workspace && cd ~/workspace
+pip install -Iv ansible
+
 git clone http://github.com/openshift/openshift-ansible
-git clone http://github.com/gshipley/installcentos
+
+cd openshift-ansible
+git checkout release-3.7
+cd ..
+
+git clone $SCRIPT_REPO
 
 cat <<EOD > /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 console console.${DOMAIN}
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 console console.${DOMAIN} $(hostname)
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
 EOD
 
-cat /dev/zero | ssh-keygen -t rsa -q -N ""
-ssh-copy-id root@console.${DOMAIN}
+if [ -z $DISK ]; then 
+	echo "Not setting the Docker storage."
+else
+	echo DEVS=$DISK >> /etc/sysconfig/docker-storage-setup
+	echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
+	echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
+	echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
 
-######################################################################################
-## THIS IS HIGHLY INSECURE AND ONLY ACCEPTABLE IN DEVELOPMENT
-echo 'INSECURE_REGISTRY="--insecure-registry 172.30.0.0/16"' >> /etc/sysconfig/docker
-######################################################################################
+	systemctl stop docker
 
+	rm -rf /var/lib/docker
+	wipefs --all $DISK
+	docker-storage-setup
+fi
 
-#################################################################################
-## THIS IS HIGHLY INSECURE AND ONLY ACCEPTABLE IN DEVELOPMENT
-#echo 'INSECURE_REGISTRY="--insecure-registry"' >> /etc/sysconfig/docker
-#################################################################################
-
-########################################################################################################
-#See: http://blog.hashbangbash.com/2014/11/docker-devicemapper-fix-for-device-or-resource-busy-ebusy/
-cp -p /usr/lib/systemd/system/docker.service /usr/lib/systemd/system/docker.service.ORIGINAL
-## NO, IT DOES NOT WORK:: sed 's/MountFlags=slave/MountFlags=private/' -i /usr/lib/systemd/system/docker.service
-########################################################################################################
-
-
-systemctl restart docker
+systemctl start docker
 systemctl enable docker
 
-cd ~/workspace
-cat installcentos/inventory.erb | sed "s/techdope.io/${DOMAIN}/g" > /tmp/installcentos-inventory.erb
-ansible-playbook -i /tmp/installcentos-inventory.erb openshift-ansible/playbooks/byo/config.yml
+ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no root@localhost "exit"
 
-#################################################################
-##            PLEASE CHOOSE BETTER CREDENTIALS
-##
-## Note: make sure that the username you choose corresponds to
-##       an existing username in the system. Also, make sure
-##       that such user belongs to group docker, like below:
-##
-##       $ usermod -a -G docker gshipley
-##
+cat installcentos/inventory.ini | sed "s/:DOMAIN:/${DOMAIN}/g" > inventory.ini
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/byo/config.yml
+
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
-#################################################################
+oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
+
+echo "******"
+echo "* Your conosle is https://console.$DOMAIN:8443"
+echo "* Your username is $USERNAME "
+echo "* Your password is $PASSWORD "
+echo "*"
+echo "* Login using:"
+echo "*"
+echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/"
+echo "******"
+
+oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/
