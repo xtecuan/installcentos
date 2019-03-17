3308fd3535a3c96d216baeaf528047b2369b0336
George Gastaldi <gegastaldi@gmail.com>
2019-03-16 22:02:30 -0300

    chore: simplified git clone operation
    Clone a specific branch and ignore repository history

--- install-openshift.sh
+++ install-openshift.sh
@@ -111,9 +111,7 @@ yum -y --enablerepo=epel install pyOpenSSL
 curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
 yum -y --enablerepo=epel install ansible.rpm
 
-[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
-
-cd openshift-ansible && git fetch && git checkout release-${VERSION} && cd ..
+[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git -b release-${VERSION} --depth=1
 
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
